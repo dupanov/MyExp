917776982696a360c85cfed1c39783828a14e7ec
dupanov <vadim.dupanov@gmail.com>
2015-10-30 00:11:48 +0200

    Gauss method added

--- src/MyExp.java
+++ src/MyExp.java
@@ -1,11 +1,11 @@
 /**
- * Exponent power x approximied by Fourie
+ * Exponent power x approximied by Fourie series
  * Created by Вадик on 29.10.2015.
  */
 public class MyExp {
     public static void main(String[] args) {
         for(int i=1;i<10;i++ ) {
-            System.out.print(i + "\t" + myExp(i));
+            System.out.print(i + "\t" + myExp(i) + "\t" +gauss(i));
             System.out.println("");
         }
     }
@@ -17,6 +17,20 @@ public class MyExp {
         while (exp>0.0000001){
             exp = exp*x/i;
             sum = sum + exp;
+            i++;
+        }
+        return sum;
+    }
+
+    public static double gauss (double x) {
+        double exp = 1;
+        double sum = 0;
+        int i=1;
+        while (i<100)
+                //Math.abs(exp)>0.000000000000001)
+                {
+            sum = sum + exp;
+            exp = -1*exp*(x*x)/i;
             i++;
         }
         return sum;
